ccc05f31427d11d5bc9f5380b9d7ee0a7bc992c5
brobo1 <armanahz95@gmail.com>
2024-03-14 13:25:23 +0100

    Refactor Navbar component for smooth scrolling
    The Navbar component was adjusted to include smooth scrolling using the react-scroll library. Further, the hover effect implementation was updated to utilize state and effect hooks, creating more dynamic and responsive hover effects. Additionally, adjustments were made to CSS styles to enhance component aesthetics.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,73 +1,98 @@
+import { Link } from "react-scroll";
 import styles from "./Navbar.module.css";
 import { motion, useAnimation } from "framer-motion";
+import { useEffect, useState } from "react";
 
 const navLinks: { page: string; link: string }[] = [
-  { page: "about", link: "/#about" },
-  { page: "projects", link: "/#projects" },
+  { page: "about", link: "about" },
+  { page: "projects", link: "projects" },
 ];
 
 export function Navbar() {
   return (
-    <>
-      <nav className={styles.nav}>
-        <ul>
-          {navLinks.map((link) => (
-            <>
-              <NavbarItem key={link.link} text={link.page} />
-            </>
-          ))}
-        </ul>
-      </nav>
-    </>
+    <nav className={styles.nav}>
+      <ul>
+        {navLinks.map((link) => (
+          <NavbarItem key={link.link} text={link.page} />
+        ))}
+      </ul>
+    </nav>
   );
 }
 
 function NavbarItem(props: { text: string }) {
   const controls = useAnimation();
+  const liControls = useAnimation();
+  const [isHovered, setIsHovered] = useState(false);
+  const [isActive, setIsActive] = useState(false);
+
   const navSelector = 40;
 
-  function startHover() {
-    controls.start({
-      width: `${navSelector * 2}px`,
-    });
-  }
+  const startHover = () => {
+    setIsHovered(true);
+  };
 
-  function endHover() {
-    controls.start({
-      width: `${navSelector}px`,
-    });
-  }
+  const endHover = () => {
+    setIsHovered(false);
+  };
+
+  useEffect(() => {
+    if (isHovered || isActive) {
+      controls.start({
+        width: `${navSelector * 2}px`,
+      });
+      liControls.start({
+        opacity: 1,
+      });
+    } else {
+      controls.start({
+        width: `${navSelector}px`,
+      });
+      liControls.start({
+        opacity: 0.6,
+      });
+    }
+  }, [isHovered, isActive, controls, liControls]);
 
   return (
-    <motion.li
-      className={styles.div}
-      initial={{
-        opacity: 0.6,
-      }}
-      whileHover={{
-        opacity: 1,
-      }}
-      transition={{
-        type: "spring",
-        stiffness: 500,
-        damping: 40,
-      }}
-      onHoverStart={startHover}
-      onHoverEnd={endHover}
+    <Link
+      className={styles.Link}
+      to={props.text}
+      spy={true}
+      smooth={true}
+      offset={-10}
+      duration={400}
+      onMouseOver={startHover}
+      onMouseOut={endHover}
+      onSetActive={() => setIsActive(true)}
+      onSetInactive={() => setIsActive(false)}
     >
-      <motion.span
-        animate={controls}
+      <motion.li
+        animate={liControls}
+        className={styles.div}
         initial={{
-          width: `${navSelector}px`,
+          opacity: 0.6,
         }}
         transition={{
           type: "spring",
           stiffness: 500,
-          damping: 20,
+          damping: 40,
         }}
-        className={styles.line}
-      />
-      {props.text}
-    </motion.li>
+      >
+        <motion.span
+          animate={controls}
+          initial={{
+            width: `${navSelector}px`,
+          }}
+          transition={{
+            type: "spring",
+            stiffness: 500,
+            damping: 20,
+          }}
+          className={styles.line}
+        />
+        {props.text}
+      </motion.li>
+    </Link>
   );
 }
