1dd94cc6ec3111cdc92d4adf41216637aec89886
brobo1 <armanahz95@gmail.com>
2024-11-14 12:15:04 +0100

    Add Vercel Analytics to App and update dependencies
    - Imported and integrated Vercel Analytics in `App.tsx`.
    - Updated various dependencies in `package-lock.json` including esbuild and rollup.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import "./App.css";
 import { Home } from "./components/Home.tsx";
+import { Analytics } from "@vercel/analytics/react";
+
 function App() {
-  return <Home />;
+  return (
+    <>
+      <Home />
+      <Analytics />
+    </>
+  );
 }
 
 export default App;
