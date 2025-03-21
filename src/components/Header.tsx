04f1b7e79227bcd95b09c2945d2b22d0dc552d7d
Brobo1 <armanahz95@gmail.com>
2025-03-21 18:06:46 +0100

    Update Header component with new name and title
    Revised the displayed name to "Arman Berg" and updated the job title to "Junior fullstack developer" for accuracy. Removed unused `awLink` import to clean up the code.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,13 @@
 import { Navbar } from "./Navbar.tsx";
 import styles from "./Header.module.css";
 import { Socials } from "./Socials.tsx";
-import { awLink } from "./reuse/consts.tsx";
 
 export function Header() {
   return (
     <div className={styles.div}>
       <div>
-        <h1>Arman Ahmadzadeh</h1>
-        <p>Junior fullstack consultant at {awLink}</p>
+        <h1>Arman Berg</h1>
+        <p>Junior fullstack developer</p>
         <Navbar />
       </div>
 
